509bcfddb565cb69d0d565ecd56cd2691b9a385e
levblanc <levblanc@gmail.com>
2023-01-19 16:42:37 +0800

    Init repo

---
+++ index.ts
@@ -0,0 +1,30 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+  type Query {
+    cars: [Car!]!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    cars: () => [{ id: 1, color: 'blue', make: 'Toyota' }],
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
